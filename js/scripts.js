38ae5101f3638fb7119fb26005db73260d7b9ff0
rmdg82 <romeo_digregorio@hotmail.com>
2020-05-08 10:17:34 +0200

    Changes on directories + implement animated scoll from navbar

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,20 @@
 /* Some scripts */
 
-/* Enable Tooltips*/
 $(function () {
-    $('[data-toggle="tooltip"]').tooltip()
-})
+    "use strict";
+
+    /* Enable Tooltips */
+    $('[data-toggle="tooltip"]').tooltip();
+
+    /* Scroll to section from .js-scroll-trigger */
+    $('.js-scroll-trigger').click(function () {
+        var sectionTo = $(this).attr('href');
+        console.log(sectionTo);
+        $('html, body').animate({
+            scrollTop: $(sectionTo).offset().top - 50
+        }, 1500);
+    });
+
+    /* Scroll spy */
+
+});
